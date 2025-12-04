4fc470013487b0d2a8b038c6a829f9ab11b8d885
Anes <tokovic.anes@gmail.com>
2025-12-04 19:27:34 +0100

    fix

---
+++ Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, Stylesheet, View } from "react-native";
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>This is Main Screen.</Text>
+    </View>
+  );
+};
+
+const styles = Stylesheet.create({});
